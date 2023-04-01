879cd9089134ac23faa3a9cb808750d16c84699d
Shivam Aery <shivamaeryy08@gmail.com>
2023-04-01 10:03:00 -0400

    Write first program

---
+++ DataTypesVariablesNotes/firstProgram.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // loads prebuilt libraries, io stream is for printing for console
+
+int main(){ // Main function, starting point of c++ program or entry point of program
+    std::cout << "Hello World in C++20!" << std::endl; // print statement, cannot do without iostrem, endl means newline
+    std::cout << "Number1"; //Simisalr to system.oput.print in java, no newline wihtout endl
+    std::cout <<"Number2";
+    return 0; // main function sneding message to os if it finished succesffuly 0 = success, otherwise problem
+}
