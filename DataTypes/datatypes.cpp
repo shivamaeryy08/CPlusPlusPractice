9f0f00b6c52c4b18b2692b0e074506adcac8f3b7
Shivam Aery <shivamaeryy08@gmail.com>
2023-04-01 15:34:06 -0400

    notes on auto

--- DataTypes/datatypes.cpp
+++ DataTypes/datatypes.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <iomanip> // for using std set precision
 
 // auto is for deducing other types
 // everything in a program, is jst a bumnc of 1's and 0's in computer's ram
@@ -21,7 +22,7 @@ int modifyingIntegers() {
     // short makes the int 2 bytes instead, long is 8 bytes 
     // can only use signed unsigned long short on ints
     // long is 4 bytes if not used as modifier, short 2 bytes
-    unsigned long long positive_number {4};
+    unsigned long long positive_number {4L};
     std::cout << "The size of a long long in bytes is: " << sizeof(positive_number) << std::endl;
     return positive_number;
 }
@@ -38,6 +39,9 @@ int main () {
     // when printing, C++ auto converts these non decimal numbers to decimal values
     std::cout << "The size of an int in bytes is: " << sizeof(int) << std::endl;
     modifyingIntegers();
+    char D {68};
+    std::cout << "The letter D is: " << D << std::endl;
+    auto number {1ul}; //can add suffix such as ul for unsigned long,  u = unsigned, ll = long long
     return 0;
 }
 
@@ -57,4 +61,35 @@ THREE TYPES BRACED INITLIZATION FUNCTIONAL INITLIZATION OR ASSIGNEMNT INITLIZATI
 
 can use sizeof to get number of byts something occupies
 
+*/
+
+
+/*
+FRACTIONAL NUMBERS
+float SIZE = 4 precision = 7
+double SIZE = 8 precision = 15
+long double = 12 so 1.5*size of double, double has more precision than float
+precision = number of digits
+can do std::cout << std::setprecision(20) to change precision of outputs
+can use scitneitic notaiton with 3e-11 for example with floats
+can divide by 0 with 0 and get infiintie or - inf and can do 0.0/0.0 which is NaN
+double is deafult like in java, float add suffix, lojng add L suffix
+*/
+
+/*
+BOOLEANS
+printing bool gives 1 for true 0 for false
+can use std::cout << std::boolalpha to force true and false to print 
+boolean is 1 byte
+
+*/
+
+/*
+CHAR
+1 byte is 256 values, a char is 1 byte
+
+*/
+
+/*
+AUTO lets compiler deduce the type 
 */
