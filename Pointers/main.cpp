ea8120bbc07c17513066964f16f373a922493f29
Shivam Aery <shivamaeryy08@gmail.com>
2023-05-09 06:51:03 -0400

    practice

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -17,8 +17,37 @@ int *pointer_to_null{nullptr};
 
 // all pointers of any type have the same size, since they all store addresses
 
-//
+// initializing poitners 
 
+int int_var {43};
+int *p_int_var{&int_var}; // the address of operator (&)
+
+std::cout << "p_int (Address in memory) : " << p_int_var << std::endl;
+
+// Derefencing a pointer : if we have the address we can read the value at the address
+std::cout << "Address: " << p_int_var << " Value: " << *p_int_var << std::endl;
+
+int_var = 400;
+std::cout << "Address: " << p_int_var << " Value: " << *p_int_var << std::endl;
+
+// pointer to char
+
+char char_var {'A'};
+
+char *char_ptr{&char_var};
+
+std::cout << "Value of char_ptr is: " << *char_ptr << std::endl;
+
+// can initilaize char pointer to a string literal c-string
+
+// the c string will be split into a char array and the first character will be pointed to by our pointer
+
+// must use const for this as the char array will be made to be const by compilers
+
+
+const char *message {"Hello World"};
+
+std::cout << "Message ptr value: " << *message << std::endl;
 
 
 }
